bb9045488ceb65c4b406729462ff2873564575bb
Poosstoo <pieu91@gmail.com>
2019-02-25 14:39:01 +0530

    Add files via upload

---
+++ array_rev.py
@@ -0,0 +1,31 @@
+'''-----Arrays - DS: An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  (you may also see it written as ).
+
+Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.------'''
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    n=len(a)
+    return a[n::-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
